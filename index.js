91e01b9432ecdc66479d0fe70c49ac104c53642c
pranjul jangra <pranjuljan68@gmail.com>
2025-03-29 11:33:51 +0530

    resolve deprecated options

--- index.js
+++ index.js
@@ -15,10 +15,7 @@ app.use(cors());
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
